141f3fa59f571c12fc7577860ef1762c59d3ed56
scp100 <66261032+scp100@users.noreply.github.com>
2025-04-23 01:17:06 +0800

    Update Main.java

--- src/main/java/tw/ntou/pettracker/Main.java
+++ src/main/java/tw/ntou/pettracker/Main.java
@@ -1,1 +1,24 @@
+package tw.ntou.pettracker;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        root.setLeft(new Label("【虛擬寵物區】"));
+        root.setRight(new Label("【任務管理區】"));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("虛擬寵物伴你養成好習慣");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
 
