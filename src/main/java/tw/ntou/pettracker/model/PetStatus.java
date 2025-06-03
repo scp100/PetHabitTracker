4d1db306192edc7412a4a00a10d3b340d14dcd14
Yunki746630 <139089337+Yunki746630@users.noreply.github.com>
2025-06-04 04:33:26 +0800

    Add files via upload

---
+++ src/main/java/tw/ntou/pettracker/model/PetStatus.java
@@ -0,0 +1,9 @@
+package tw.ntou.pettracker.model;
+
+public class PetStatus {
+    private String lastRewardDate;
+
+    public String getLastRewardDate() {return lastRewardDate;}
+
+    public void setLastRewardDate(String lastRewardDate) {this.lastRewardDate = lastRewardDate;}
+}
